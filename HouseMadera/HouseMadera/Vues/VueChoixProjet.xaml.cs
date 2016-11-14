d3c6aa1aa7a9aebc9daeed665c4eab8d8f0049d4
Paul <paul.marquie@viacesi.fr>
2016-11-14 10:39:46 +0100

    Création packages Création vue choix projet

---
+++ HouseMadera/HouseMadera/Vues/VueChoixProjet.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace HouseMadera.Vues
+{
+    /// <summary>
+    /// Logique d'interaction pour VueChoixProjet.xaml
+    /// </summary>
+    public partial class VueChoixProjet : Window
+    {
+        public VueChoixProjet()
+        {
+            InitializeComponent();
+        }
+    }
+}
