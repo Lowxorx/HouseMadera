0667275f731b89a86c9d0ac1b80a8a6b1cc138fd
remy delmas <remy.cool@hotmail.fr>
2017-02-06 18:38:41 +0100

    Validation des données formulaire client

--- HouseMadera/HouseMadera/Vue-Modele/ClientViewModel.cs
+++ HouseMadera/HouseMadera/Vue-Modele/ClientViewModel.cs
@@ -1,40 +1,40 @@
-﻿using System;
-
+﻿
+using System;
+using System.Text.RegularExpressions;
 
 namespace HouseMadera.Vue_Modele
 {
-    public class ClientViewModel :System.ComponentModel.IDataErrorInfo
+    public class ClientViewModel 
     {
-        public string Nom { get; set; }
+        private string _nom;
+        public string Nom {
+            get { return 
+                    _nom; }
+            set {
+                var match = Regex.Match(value, @"\d+");
+                if (match.Success)
+                    throw new ApplicationException("Le nom ne doit pas contenir de chiffre");
+            }
+        }
+        private string _prenom;
         public string Prenom { get; set; }
+        private string _numero;
         public int Numero { get; set; }
+        private string _voie;
         public string Voie { get; set; }
+        private string _nomvoie;
         public string NomVoie { get; set; }
+        private string _complement;
         public string Complement { get; set; }
+        private int _codepostal;
         public int CodePostal { get; set; }
+        private string _localite;
         public string Localite { get; set; }
+        private string _mobile;
         public string Mobile { get; set; }
+        private string _telephone;
         public string Telephone { get; set; }
+        private string _email;
         public string Email { get; set; }
-
-        public string Error
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public string this[string columnName]
-        {
-            get
-            {
-                switch (columnName)
-                {
-                    //case "CodePostal":
-                }
-                throw new NotImplementedException();
-            }
-        }
     }
 }
--- HouseMadera/HouseMadera/Vues/Client.xaml.cs
+++ HouseMadera/HouseMadera/Vues/Client.xaml.cs
@@ -7,6 +7,7 @@ using System.Text.RegularExpressions;
 using System.Drawing;
 using System;
 using HouseMadera.Vue_Modele;
+using HouseMadera.Utilites;
 
 namespace HouseMadera.Vues
 {
@@ -16,13 +17,15 @@ namespace HouseMadera.Vues
     public partial class Client : MetroWindow
     {
 
-       
+        //  public Dictionary<int,string> Localite { get; set; }
 
-
+        public RegexUtilities regle { get; set; }
         public Client()
         {
             InitializeComponent();
             this.DataContext = new ClientViewModel();
+            // Localite = new Dictionary<int, string>();
+            regle = new RegexUtilities();
           
         }
 
@@ -80,17 +83,102 @@ namespace HouseMadera.Vues
                 Console.WriteLine("OK");
         }
 
-        private void textBox_Voie_TextChanged(object sender, TextChangedEventArgs e)
+        private void textBox_Numero_TextChanged(object sender, TextChangedEventArgs e)
         {
             //La voie
-            var match = Regex.Match(textBox_Nom.Text, @"^\d*\s?(bis|ter)?$");
+            var match = Regex.Match(textBox_Numero.Text, @"^\d*\s?(bis|ter)?$");
             if (match.Success)
                 //TODO afficher l'erreur
                 Console.WriteLine("Voie OK");
             else
-                Console.WriteLine("Le format de la voie est oncorrect");
+                Console.WriteLine("Le format de la voie est incorrect");
         }
 
 
+        private void textbox_Mobile_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var isOk = regle.IsValidTelephoneNumber(textBox_Mobile.Text);
+            if (isOk)
+                Console.WriteLine("Mobile OK");
+            else
+                Console.WriteLine("Format incorect du numero de mobile");
+
+        }
+
+        private void textbox_Telephone_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var isOk = regle.IsValidTelephoneNumber(textBox_Telephone.Text);
+            if (isOk)
+                Console.WriteLine("Telephone OK");
+            else
+                Console.WriteLine("Format incorect du numero de telephone");
+
+        }
+
+        private void textbox_Email_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var isOk = regle.IsValidEmail(textBox_Email.Text);
+            if (isOk)
+                Console.WriteLine("Email OK");
+            else
+                Console.WriteLine("Format incorect de l'e-mail");
+
+        }
+
+        private void RechercherCommune(object sender, TextChangedEventArgs e)
+        {
+            int i;
+            var isCodePostal = int.TryParse(textBox_CodePostal.Text.ToString(), out i);
+
+
+            var communes = new List<Modeles.Commune>();
+            //TODO modifier "SQLITE" par Bdd
+            if(textBox_CodePostal.Text != string.Empty && isCodePostal)
+            {
+                using (var dal = new CommuneDAL("SQLITE"))
+                {
+                    communes = dal.GetFilteredCommunes(Convert.ToInt32(textBox_CodePostal.Text));
+                }
+            }
+           
+            if(communes.Count>0)
+            {
+               var communesSuggestionList = new List<string>();
+               
+                foreach(var commune in communes)
+                {
+                    var suggestion = commune.ToString();
+                    communesSuggestionList.Add(suggestion);
+                    
+                }
+                ListBox_Suggestion.ItemsSource = communesSuggestionList;
+                ListBox_Suggestion.Visibility = Visibility.Visible;
+            }
+            else if (string.IsNullOrEmpty(textBox_CodePostal.Text))
+            {
+                ListBox_Suggestion.ItemsSource = null;
+                ListBox_Suggestion.Visibility = Visibility.Collapsed;
+            }
+        }
+
+       private void RemplirLocalite(object sender, SelectionChangedEventArgs e)
+        {
+            if(ListBox_Suggestion.ItemsSource != null)
+            {
+                ListBox_Suggestion.Visibility = Visibility.Collapsed;
+                if (ListBox_Suggestion.SelectedItem != null)
+                {
+                    var communeSelected = ListBox_Suggestion.SelectedItem.ToString();
+                    var vm = new CommuneViewModel(communeSelected);
+                    textBox_Localite.Text = string.IsNullOrEmpty(vm.NomCommune) ? string.Empty : vm.NomCommune;
+                    textBox_CodePostal.Text = string.IsNullOrEmpty(vm.CodePostal) ? string.Empty : vm.CodePostal;
+                    ListBox_Suggestion.Visibility = Visibility.Collapsed;
+                }
+            }
+            else
+                textBox_Localite.Text = string.Empty;
+        }
+
+       
     }
 }
