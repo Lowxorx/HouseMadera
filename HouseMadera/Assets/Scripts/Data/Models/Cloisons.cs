99c9d10925441c88d51bd223e751abe73cece596
Nicolas <nicolas.bonnet@viacesi.fr>
2017-03-08 17:13:23 +0100

    Cloison.cs added

---
+++ HouseMadera/Assets/Scripts/Data/Models/Cloisons.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloisons  {
+
+	public string name { get; set; }
+    public int vertical { get; set; }
+    public int horizontal { get; set; }
+
+    public Cloisons()
+    {
+        name = "";
+        vertical = 0;
+        horizontal = 0;
+    }
+	
+}
